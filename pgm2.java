93658d08ac9e8a80666437d5c9d12f0315d2f8b7
youngwolf <102538684+manikandans007@users.noreply.github.com>
2022-10-24 22:22:07 +0530

    Add files via upload

---
+++ pgm2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class inputprogram{
+   public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("enter ur first name :");
+    String firstname=input.next();
+    System.out.println("your full name is "+firstname);
+   } 
+}
